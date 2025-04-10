9ffc8e3e2bd74a4612115a01003d45cb78982af5
Your Name <niklas1sandberg@gmail.com>
2025-04-10 17:26:53 +0200

    Bugg, Jumpy Encoder Values: Another implementation, this is slower but more solid. Maybe work with this if RPi switch does not work

--- Code/nebulae/encoder.py
+++ Code/nebulae/encoder.py
@@ -1,38 +1,27 @@
 # encoder.py
-# Stephen Hensley
-# Encoder Class Adapted from py-gaugette rotary encoder library
 
 import RPi.GPIO as GPIO
 import time
 import math
 import threading
 
+# Clockwise sequence for your encoder: 0 -> 1 -> 3 -> 2 -> 0
+SEQ = [0, 1, 3, 2]
+SEQ_LEN = len(SEQ)
+
 class Encoder:
 
-    # State transition table for full-step state machine
-    _state_machine = {
-        (0, 1): +1,
-        (1, 3): +1,
-        (3, 2): +1,
-        (2, 0): +1,
-
-        (0, 2): -1,
-        (2, 3): -1,
-        (3, 1): -1,
-        (1, 0): -1,
-    }
-
     def __init__(self, pin_a, pin_b):
-        self.pin_a = pin_b; # Our Encoders are inverted,
-        self.pin_b = pin_a; # Which is why this looks weird
+        self.pin_a = pin_a
+        self.pin_b = pin_b
+        self.steps = 0
+        self._seq_index = 0
+        self._last_time = time.time()
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(self.pin_a, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self.pin_b, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-        self._last_state = self.rotation_state()
-        self._last_time = time.time()
-        self.steps = 0
 
     def rotation_state(self):
         a_state = GPIO.input(self.pin_a)
@@ -41,11 +30,18 @@ class Encoder:
         return r_state
 
     def update(self):
-        new_state = self.rotation_state()
-        transition = (self._last_state, new_state)
-        if transition in self._state_machine:
-            self.steps += self._state_machine[transition]
-            self._last_state = new_state
+        state = self.rotation_state()
+        next_state = SEQ[(self._seq_index + 1) % SEQ_LEN]
+        prev_state = SEQ[(self._seq_index - 1) % SEQ_LEN]
+
+        if state == next_state:
+            self._seq_index = (self._seq_index + 1) % SEQ_LEN
+            if self._seq_index == 0:
+                self.steps += 1
+        elif state == prev_state:
+            self._seq_index = (self._seq_index - 1) % SEQ_LEN
+            if self._seq_index == 0:
+                self.steps -= 1
 
     def _isr(self, channel):
         now = time.time()
