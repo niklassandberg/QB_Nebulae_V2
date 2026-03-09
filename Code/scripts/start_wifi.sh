8ca4f03ec82a06b6036ce29129071f2f5ccaec2c
Niklas Sandberg <niklas1sandberg@gmail.com>
2026-03-09 05:08:22 +0100

    Now rpi4 works as a access point

--- Code/scripts/start_wifi.sh
+++ Code/scripts/start_wifi.sh
@@ -9,7 +9,7 @@ then
   ( sleep 10; systemctl start systemd-resolved; netctl start wlan0-wifi; iwconfig >> /home/alarm/wifi.log 2>&1 ) & 
 else
   echo starting access point > /home/alarm/wifi.log
-  ( sleep 10; systemctl start systemd-resolved; sh /home/alarm/QB_Nebulae_V2/Code/scripts/create_ap.sh --no-virt -n wlan0 Nebulae eurorack >> /home/alarm/wifi.log 2>&1 ) & 
+  ( sleep 10; systemctl start systemd-resolved; sudo netctl stop-all; sudo pkill -f "wpa_supplicant.*wlan0"; sudo pkill -f "wpa_cli.*wlan0"; sleep 2; sh /home/alarm/QB_Nebulae_V2/Code/scripts/create_ap.sh --no-virt -c 6 -n wlan0 Nebulae eurorack >> /home/alarm/wifi.log 2>&1 ) &
 fi
 
 umount /dev/sda1
